4a64796795fbb3c857762574341ebf50f225af7f
skaffery <skaffery@gmail.com>
2021-05-19 21:45:02 +0300

    Modifed main.cpp

--- main.cpp
+++ main.cpp
@@ -11,7 +11,7 @@ int main(void)
         return -1;
 
     /* Create a windowed mode window and its OpenGL context */
-    window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
+    window = glfwCreateWindow(640, 480, "WillEngine", NULL, NULL);
     if (!window)
     {
         glfwTerminate();
